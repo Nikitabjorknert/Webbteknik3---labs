d49dbeb7a76889e37878215942bb34c700a44251
Nikita Björknert <nb222tx@student.lnu.se>
2026-01-28 21:05:33 +0100

    remove

--- 1a/rectangles.js
+++
@@ -1,8 +0,0 @@
-export function rectangleArea(width, height) {
-  return width * height;
-}
-
-// Funktion för att beräkna omkrets av en rektangel
-export function rectanglePerimeter(width, height) {
-  return 2 * (width + height);
-}
