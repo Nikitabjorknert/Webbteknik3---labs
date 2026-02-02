cab31a510cf477acb95a37117642271d99d78aed
Nikita Björknert <nb222tx@student.lnu.se>
2026-02-02 09:34:28 +0100

    localStorage

--- 2c/index.js
+++ 2c/index.js
@@ -10,3 +10,18 @@ if (savedName) {
 nameInput.addEventListener("input", function() {
     localStorage.setItem("name", nameInput.value);
 });
+
+const themeRadios = document.querySelectorAll('input[name="theme"]');
+let savedTheme = "light";
+if (localStorage.getItem('selectedTheme')) {
+    savedTheme = localStorage.getItem('selectedTheme');
+}
+document.body.className = savedTheme + '-theme';
+document.querySelector(`input[value="${savedTheme}"]`).checked = true;
+
+themeRadios.forEach(radio => {
+    radio.addEventListener('change', function() {
+        localStorage.setItem('selectedTheme', this.value);
+        document.body.className = this.value + '-theme';
+    });
+});
